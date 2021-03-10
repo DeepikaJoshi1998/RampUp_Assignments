e307663cbba2ebd189873fe0fc7c5f1844facf29
Deepika joshi <44704971+DeepikaJoshi1998@users.noreply.github.com>
2021-03-10 17:08:55 +0530

    Delete Retrievedata.sql

--- Retrievedata.sql
+++
@@ -1,9 +0,0 @@
-DELIMITER //
-CREATE PROCEDURE Retreive()
-  BEGIN
-    SELECT user.id,user.name,user.age,user.status,address.city,address.state,address.country,contact.phonenumber,contact.emailid
-    FROM user
-    INNER JOIN address ON address.id = user.id    #join using foreign key
-    INNER JOIN contact ON contact.id = user.id;
-  END //
-DELIMITER ;
